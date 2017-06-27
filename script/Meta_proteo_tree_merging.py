749b2f5eb81cbb7c1af9c9b6fe06b1fc4c9fc44b
YannickCogne <yannick.cogne@hotmail.fr>
2017-06-27 12:30:45 +0200

    Add files via upload

--- script/Meta_proteo_tree_merging.py
+++ script/Meta_proteo_tree_merging.py
@@ -1,90 +1,47 @@
 #!/usr/local/bin/python
 
 from optparse import OptionParser
-
+import sys
 import json
 
 
-with open('../test_data/Chukchi_Sea.d3_hierarchy') as data_file2:    
-    data = json.load(data_file2)
-
-
-with open('../test_data/Bering_Strait.d3_hierarchy') as data_file:    
-    data2 = json.load(data_file)
-
-    
-dico={}
-dico[data["id"]]={}
-dico[data["id"]]["name"]=data["name"]
-dico[data["id"]]["data"]=data["data"]
-dico[data["id"]]["kids"]=set(data["kids"])
-
-
+#A recursive parser to construct my own data structure and easier count the peptides
 def recursparser(data,dico):
-    for i in data["children"]:
-        
+    for i in data["children"]:        
         if i["id"] not in dico.keys(): 
-            dico[i["id"]]={}
-             
-            listseq=list(i["data"]["sequences"])
+            dico[i["id"]]={}             
+            listseq=list(i["data"]["sequences"])            
             dico[i["id"]]["name"]=i["name"]
             dico[i["id"]]["data"]=i["data"]
             dico[i["id"]]["data"]["sequences"]=set()
+#set structure here is to have only non duplicate peptides when I run another data parsing
             for seq in listseq:
-                
                 dico[i["id"]]["data"]["sequences"].add(seq)
             dico[i["id"]]["kids"]=set(i["kids"])
+#set structure here is to have only non duplicate kids when I run another data parsing
             dico[i["id"]]["data"]["self_count"]=len(dico[i["id"]]["data"]["sequences"])            
             dico[i["id"]]["data"]["count"]=dico[i["id"]]["data"]["self_count"]
-
-
-        else:
-    
-             
-            listseq=list(i["data"]["sequences"])
-            
+#initialize count with only self_count peptide
+        else:                 
+            listseq=list(i["data"]["sequences"])            
             for seq in listseq:
-                
                 dico[i["id"]]["data"]["sequences"].add(seq)
             for kid in i["kids"]:
-                dico[i["id"]]["kids"].add(kid)
-            
+                dico[i["id"]]["kids"].add(kid)            
             dico[i["id"]]["data"]["self_count"]=len(dico[i["id"]]["data"]["sequences"])            
             dico[i["id"]]["data"]["count"]=dico[i["id"]]["data"]["self_count"]
-
-
-
-        if len(i["kids"]):
-                
+        if len(dico[i["id"]]["kids"]):
+#recursive method which allows to search all child of each parents                
             dico=recursparser(i,dico)
-            for k in i["kids"]:
-                    
-                dico[i["id"]]["data"]["count"]+=dico[k]["data"]["count"]
-                    
-                
-                    
-            
+            tot=0
+        
+        for k in dico[i["id"]]["kids"]:
+            dico[i["id"]]["data"]["count"]+=dico[k]["data"]["count"]
+#incremente peptides count with the count of all kids , for non parents count is self_count and for parent is the sum of first generation child count            
                      
     return dico
-dico2=recursparser(data,dico)
-#print(dico2[1236])
-for kid in data2["kids"]:
-    dico2[1]["kids"].add(kid)
-dico3=recursparser(data2,dico2)
-dico3[1]["data"]["count"]=len(dico3[1]["data"]["sequences"])
-for k in dico3[1]["kids"]:
-    dico3[1]["data"]["count"]+=dico3[k]["data"]["count"]
 
-#print (dico2[1236])
-#print (dico3 [33090])
-#print (dico3[4751])
-def dicoreader(dico,childlist):
-	for i in childlist:
-            print (dico[i]["name"])
-            print (dico[i]["data"]["count"])
-            if len(dico[i]["kids"]):
-                dicoreader(dico,dico[i]["kids"])
-
+#the function that allow to generate a d3_hierarchy format string with a recursive method
 def jsoncreator(dico,dicopos,jsonout):
     
     dico[dicopos]["data"]["sequences"]=list(dico[dicopos]["data"]["sequences"])
@@ -99,17 +56,35 @@ def jsoncreator(dico,dicopos,jsonout):
             jsonout=jsoncreator(dico,k,jsonout)+","
     jsonout+='], "id": '+str(dicopos)+', "name" : "'+dico[dicopos]["name"]+'"}'
 
-    return jsonout
-jsonstr=""
-print(jsoncreator(dico3,1,jsonstr))
-#print( '{"data": {"count": '+str(list(dico3[1236]["data"]["sequences"])).replace("'",'"'))
+    return jsonout    
+
+
+if len(sys.argv) >2:
+    with open(sys.argv[1]) as data_file:    
+        data = json.load(data_file)
+    
+    dico={}
+    dico[data["id"]]={}
+    dico[data["id"]]["name"]=data["name"]
+    dico[data["id"]]["data"]=data["data"]
+    dico[data["id"]]["kids"]=set(data["kids"])
+    dico=recursparser(data,dico)
 
     
+    for arg in sys.argv[2:]:
+        with open(arg) as data_file:    
+            data2 = json.load(data_file)
+        
+        for kid in data2["kids"]:
+            dico[1]["kids"].add(kid)
+        dico=recursparser(data2,dico)
+        dico[1]["data"]["count"]=dico[1]["data"]["self_count"]
+        for k in dico[1]["kids"]:
+            dico[1]["data"]["count"]+=dico[k]["data"]["count"]
+    
+    jsonstr=""
+    print(jsoncreator(dico,1,jsonstr))
 
-
-#str+=', "rank": "'++'", "valid_taxon": 1}, "kids":'++', "children": ['
-#dicoreader(dico2,dico2[1]["kids"])
-
-#print (len(dico))
-#print (len(dico2))
-
+        
+else :
+    print ("Argument is needed")
