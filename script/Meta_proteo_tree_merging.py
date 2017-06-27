efcc4d95b42464f7b4b861edf3b9d4042326a838
YannickCogne <yannick.cogne@hotmail.fr>
2017-06-27 10:09:34 +0200

    Add files via upload

--- script/Meta_proteo_tree_merging.py
+++ script/Meta_proteo_tree_merging.py
@@ -17,7 +17,7 @@ dico={}
 dico[data["id"]]={}
 dico[data["id"]]["name"]=data["name"]
 dico[data["id"]]["data"]=data["data"]
-dico[data["id"]]["kids"]=data["kids"]
+dico[data["id"]]["kids"]=set(data["kids"])
 
 
 def recursparser(data,dico):
@@ -67,11 +67,17 @@ def recursparser(data,dico):
                      
     return dico
 dico2=recursparser(data,dico)
-print(dico2[1236])
+#print(dico2[1236])
+for kid in data2["kids"]:
+    dico2[1]["kids"].add(kid)
 dico3=recursparser(data2,dico2)
-print (dico2[1236])
-print (dico3 [33090])
-print (dico3[4751])
+dico3[1]["data"]["count"]=len(dico3[1]["data"]["sequences"])
+for k in dico3[1]["kids"]:
+    dico3[1]["data"]["count"]+=dico3[k]["data"]["count"]
+
+#print (dico2[1236])
+#print (dico3 [33090])
+#print (dico3[4751])
 def dicoreader(dico,childlist):
 	for i in childlist:
             print (dico[i]["name"])
@@ -79,9 +85,24 @@ def dicoreader(dico,childlist):
             if len(dico[i]["kids"]):
                 dicoreader(dico,dico[i]["kids"])
 
-#def jsoncreator(dico):
+def jsoncreator(dico,dicopos,jsonout):
+    
+    dico[dicopos]["data"]["sequences"]=list(dico[dicopos]["data"]["sequences"])
+    jsonout+='{"data": '+str(dico[dicopos]["data"]).replace("'",'"')
+    jsonout+=',"kids": '+str(list(dico[dicopos]["kids"]))
+    jsonout+=', "children": ['
+    
+    for k in dico[dicopos]["kids"]:
+        if k==list(dico[dicopos]["kids"])[len(dico[dicopos]["kids"])-1]:
+            jsonout=jsoncreator(dico,k,jsonout)
+        else:
+            jsonout=jsoncreator(dico,k,jsonout)+","
+    jsonout+='], "id": '+str(dicopos)+', "name" : "'+dico[dicopos]["name"]+'"}'
 
-print( '{"data": {"count": '+str(list(dico3[1236]["data"]["sequences"])).replace("'",'"'))
+    return jsonout
+jsonstr=""
+print(jsoncreator(dico3,1,jsonstr))
+#print( '{"data": {"count": '+str(list(dico3[1236]["data"]["sequences"])).replace("'",'"'))
 
     
 
